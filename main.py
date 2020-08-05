3447922c196c32f114a66d78d9e80f92b085fd8e
iaslannascimento <iaslan.nascimento@hotmail.com>
2020-08-05 11:21:09 -0300

    update

--- main.py
+++ main.py
@@ -22,3 +22,32 @@ print(X)
 som = MiniSom(x = 8, y = 8, input_len = 13,  sigma = 1.0, learning_rate= 0.5, random_seed= 2 )
 som.random_weights_init(X)
 som.train_random(data = X, num_iteration = 100)
+
+som._weights
+som._activation_map
+q = som.activation_response(X)
+print(q)
+#plot
+from pylab import pcolor, colorbar
+pcolor(som.distance_map().T)
+colorbar()
+
+#melhor registro
+w = som.winner(X[1])
+#marcadores das classes
+markers = ['o', 's','D']
+#cores para as classes 
+color = ['r', 'g', 'b']
+
+#Y[Y == 1] = 0
+#Y[Y == 2] = 1
+#Y[Y == 3] = 2
+
+for i,x in enumerate(X):
+    #print(i)
+    #print(x)
+    w = som.winner(x)
+    plot(w[0] +0.5,w[1]+0.5, markers[y[i]],
+         markerfacecolor = 'None', markersize = 10,
+         markeredgecolor = color[y[i]], markeredgewidth = 2)
+    
